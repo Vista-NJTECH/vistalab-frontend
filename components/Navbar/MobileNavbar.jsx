5dbd34a21a728be49b9e154dcd25c8c1f62a5b28
MR-Addict <2750417853@qq.com>
2022-12-19 13:58:35 +0800

    add animition to mobile navbar

---
+++ components/Navbar/MobileNavbar.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { AiOutlineMenu } from "react-icons/ai";
+import { RxCross1 } from "react-icons/rx";
+import { useState } from "react";
+
+import { navbarData } from "./config";
+
+function Navcard({ item }) {
+  return (
+    <Link
+      href={item.href}
+      className='w-full font-semibold text-xl text-slate-700 flex flex-row gap-2 items-center justify-start border-b-2 hover:text-theme-dark p-2 rounded-md duration-300'
+    >
+      <span>{item.icon}</span>
+      <span>{item.title}</span>
+    </Link>
+  );
+}
+
+export default function MobileNavbar() {
+  const [isExpand, setIsExpand] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => setIsExpand((prevState) => !prevState)}
+        className='lg:hidden text-xl absolute top-0 bottom-0 right-5 my-auto'
+      >
+        {isExpand ? <RxCross1 /> : <AiOutlineMenu />}
+      </button>
+      <div
+        className={`w-full flex flex-col items-start gap-4 px-5 py-5 absolute left-0 top-16 bg-white shadow-md duration-500 ${
+          isExpand ? "translate-x-0" : "translate-x-[-100%]"
+        }`}
+      >
+        {navbarData.map((item, index) => (
+          <Navcard key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,68 +1,35 @@
-"use client";
-
 import Link from "next/link";
 import Image from "next/image";
-import { AiOutlineMenu } from "react-icons/ai";
-import { RxCross1 } from "react-icons/rx";
-import { useState } from "react";
 
 import { navbarData } from "./config";
 import logo from "./logo.png";
-import useWindowSize from "../../lib/useWindowSize";
+import MobileNavbar from "./MobileNavbar";
+
+function NavbarCard({ item }) {
+  return (
+    <Link
+      href={item.href}
+      className='font-semibold text-md rounded-md text-slate-700 hover:text-theme-dark duration-300 flex flex-row gap-1 items-center justify-center'
+    >
+      <span>{item.icon}</span>
+      <span>{item.title}</span>
+    </Link>
+  );
+}
 
 export default function Navbar() {
-  const [isExpand, setIsExpand] = useState(false);
-  const windowSize = useWindowSize();
-
-  const NavbarCard = ({ item }) => {
-    return (
-      <Link
-        href={item.href}
-        className='font-semibold text-md rounded-md text-slate-700 hover:text-theme-dark duration-300 flex flex-row gap-1 items-center justify-center'
-      >
-        <span>{item.icon}</span>
-        <span>{item.title}</span>
-      </Link>
-    );
-  };
-  const handleClickExpand = () => {
-    setIsExpand((prevState) => !prevState);
-  };
-
   return (
-    <div className='frame py-3 w-full flex flex-col items-center justify-between shadow-md'>
-      <div className='flex flex-row w-full items-center justify-between'>
-        <div className='text-2xl font-bold text-slate-800 font-serif italic flex flex-row items-end justify-center gap-2'>
-          <Image src={logo} width={40} alt='logo' className='w-auto' />
-          <h1>Vistalab</h1>
-        </div>
-        {windowSize.width > 1024 && (
-          <div className='flex flex-row items-center justify-center gap-4'>
-            {navbarData.map((item, index) => (
-              <NavbarCard key={index} item={item} />
-            ))}
-          </div>
-        )}
-        {windowSize.width <= 1024 && (
-          <button className='text-xl' onClick={handleClickExpand}>
-            {isExpand ? <RxCross1 /> : <AiOutlineMenu />}
-          </button>
-        )}
+    <div className='frame py-3 shadow-md flex flex-row w-full items-center justify-between relative'>
+      <div className='text-2xl font-bold text-slate-800 font-serif italic flex flex-row items-end justify-center gap-2'>
+        <Image src={logo} width={40} alt='logo' className='w-auto' />
+        <h1>Vistalab</h1>
       </div>
-      {isExpand && windowSize.width <= 1024 && (
-        <div className='w-full flex flex-col items-start gap-4 py-2'>
-          {navbarData.map((item, index) => (
-            <Link
-              key={index}
-              href={item.href}
-              className='w-full font-semibold text-xl text-slate-700 flex flex-row gap-2 items-center justify-start border-b-2 hover:text-theme-dark p-2 rounded-md duration-300'
-            >
-              <span>{item.icon}</span>
-              <span>{item.title}</span>
-            </Link>
-          ))}
-        </div>
-      )}
+      <div className='hidden lg:flex flex-row items-center justify-center gap-4'>
+        {navbarData.map((item, index) => (
+          <NavbarCard key={index} item={item} />
+        ))}
+      </div>
+      <MobileNavbar />
     </div>
   );
 }
