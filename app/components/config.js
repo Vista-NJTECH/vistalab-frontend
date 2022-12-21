b40f2cd15f18720eed0d955d3f7d376e41dde68a
JerryGu <jerrygu.gjw@gmail.com>
2022-12-21 18:29:19 +0800

    change words

--- app/components/config.js
+++ app/components/config.js
@@ -25,18 +25,15 @@ export const stacksData = [
 
 export const awardsData = {
   2022: [
-    "Lorem ipsum dolor sit amet, consectetur adipisicdcsing elit.",
-    "Lorem ipsum dolor sis adipisci ipsum fugit aradcaiam repudiandae?",
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elisat. Tempore, officia.",
-    "Lorem ipsum dolor sit aaDSmet, consectetur adipisicing elit.",
-    "Lores adipisci ipsum SDA xasfdsa repudiandae?",
+    "2022年(第15届)中国大学生计算机设计大赛人工智能挑战赛直报赛区选拔赛(省级)一等奖.",
+    "2022年(第15届)中国大学生计算机设计大赛一等奖.",
+    "2022年第十七届全国大学生智能车竞赛航天智慧物流组南区赛三等奖.",
+    "第六届强网杯全国网络安全挑战赛“强网先锋”.",
   ],
   2021: [
-    "Lorem ipsum dolor sit amet, conscxcectetur adipisicing elt.",
-    "Lores adipisci ipsum fugit cx rexzcpudiandae?",
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, officia.",
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-    "Lores adipisci ipsum fugit xasfdsa repudiandae?",
+    "2021年第十六届全国大学生智能车竞赛航天智慧物流组北区赛二等奖.",
+    "2021年(第14届)中国大学生计算机设计大赛三等奖.",
+    "2021年全国大学生电子设计竞赛江苏省一等奖.",
   ],
 };
 
@@ -56,30 +53,30 @@ import activity04 from "./images/activities/activity04.jpeg";
 
 export const activitiesData = [
   {
-    title: "Good and nice day for joining manga!",
-    date: "2022/10/30",
-    intro: "Lorem adipisci ipsum fugit xasfdsa repudiandae?",
+    title: "我们的第一堂电控课！",
+    date: "2022/12/01",
+    intro: "有张毅杰同学主讲的电控课，开启大家极客的生涯！",
     href: "/",
     src: activity04,
   },
   {
-    title: "Nice team work! Everyone is happy!",
-    date: "2022/10/28",
-    intro: "Lorem adipisci ipsum fugit xasfdsa repudiandae?",
+    title: "我们的第一堂算法课！",
+    date: "2022/11/09",
+    intro: "由南京工业大学算法队何海嘉同学主讲的的传统算法，旨在提高同学们的编程思维与能力。",
     href: "/",
     src: activity03,
   },
   {
-    title: "Another good day for learning and teaching!",
-    date: "2022/11/22",
-    intro: "Lorem adipisci ipsum fugit xasfdsa repudiandae?",
+    title: "为比赛作准备！",
+    date: "2022/3/21",
+    intro: "在焊接车辆的部件。",
     href: "/",
     src: activity02,
   },
   {
     title: "WE DID IT! Waiting another year!",
-    date: "2022/12/10",
-    intro: "Lorem adipisci ipsum fugit xasfdsa repudiandae?",
+    date: "2021/12/18",
+    intro: "王正阳手捧起他制作的第一辆循迹小车。",
     href: "/",
     src: activity01,
   },
