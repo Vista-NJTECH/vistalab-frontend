8dc766ce8f0ad0e178030fad2a77b108e1dbdbe4
MR-Addict <2750417853@qq.com>
2022-12-27 12:41:45 +0800

    remove axios

--- app/invoice/page.jsx
+++ app/invoice/page.jsx
@@ -1,59 +1,38 @@
-'use client';
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import "../../statics/table.css";
 
-import '../../statics/table.css'
-
-const result = []
-
-const Members = () => {
-  const [data, setData] = useState([])
-  const [isLoading, setLoading] = useState(false)
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.post('http://124.223.196.177:8181/api/allinvoice')
-      setData(result.data.data)
-      setLoading(false)
-      console.log(result.data.data)
-    }
-    fetchData()
-  }, [])
-
-  if (isLoading) return <p>Loading...</p>
-  if (!data) return <p>No profile data</p>
-  
+export default async function Page() {
+  const res = await fetch("http://124.223.196.177:8181/api/allinvoice");
+  if (!res.ok) throw new Error("Failed to fetch data");
+  const data = await res.json();
   return (
-
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Invoicename</th>
-          <th>Applicant</th>
-          <th>Amount</th>
-          <th>Remark</th>
-          <th>Category</th>
-          <th>Time</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-        data.map((item) => (
-          <tr key={item.id}>
-            <td>{item.invoicename}</td>
-            <td>{item.applicant}</td>
-            <td>{item.amount}</td>
-            <td>{item.remark}</td>
-            <td>{item.category}</td>
-            <td>{item.time}</td>
-            <td>{item.status}</td>
+    <div className='frame flex flex-col items-center justify-center'>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Invoicename</th>
+            <th>Applicant</th>
+            <th>Amount</th>
+            <th>Remark</th>
+            <th>Category</th>
+            <th>Time</th>
+            <th>Status</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  )
+        </thead>
+        <tbody>
+          {data.data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.invoicename}</td>
+              <td>{item.applicant}</td>
+              <td>{item.amount}</td>
+              <td>{item.remark}</td>
+              <td>{item.category}</td>
+              <td>{item.time}</td>
+              <td>{item.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
-
-export default Members
