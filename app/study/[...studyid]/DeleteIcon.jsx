17669a17b4d9f79a1fe3199ec994dca8e12ac65c
MR-Addict <2750417853@qq.com>
2023-01-03 20:40:23 +0800

    add login auth before delete study data

--- app/study/[...studyid]/DeleteIcon.jsx
+++ app/study/[...studyid]/DeleteIcon.jsx
@@ -1,13 +1,15 @@
 "use client";
 
+import { useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useSession, signIn } from "next-auth/react";
 
 import { Popup } from "../../../components";
 
 export default function DeleteIcon({ item }) {
   const router = useRouter();
+  const { data: session } = useSession();
   const [isPopup, setIsPopup] = useState(false);
   const handleDelete = async (id) => {
     const res = await fetch("http://124.223.196.177:8181/study/delete", {
@@ -24,7 +26,7 @@ export default function DeleteIcon({ item }) {
     <div className='title text-xl font-bold group flex flex-row items-center w-full'>
       <h1>{item.title}</h1>
       <div
-        onClick={() => setIsPopup(true)}
+        onClick={() => (session ? setIsPopup(true) : signIn())}
         className='group-hover:block hidden duration-300 group-hover:text-slate-800/50 cursor-pointer'
       >
         <MdDelete />
