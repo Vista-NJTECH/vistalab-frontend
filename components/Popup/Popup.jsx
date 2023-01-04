1382c88453a2b8d566b27102958fa134f2b1755c
MR-Addict <2750417853@qq.com>
2023-01-04 14:06:42 +0800

    fix popup box size

--- components/Popup/Popup.jsx
+++ components/Popup/Popup.jsx
@@ -2,10 +2,10 @@ export default function Popup({ before, after }) {
   return (
     <div className='fixed top-0 left-0 w-screen h-screen bg-black/20 flex items-center justify-center'>
       {after.isProcessing ? (
-        <div className='w-full max-w-xs flex flex-col items-center justify-center gap-4 bg-white p-5 rounded-md'>
+        <div className='flex flex-col items-center justify-center gap-4 bg-white p-5 rounded-md'>
           <h1 className='title text-3xl'>{after.message}</h1>
           {after.message !== "Processing..." && (
-            <button onClick={() => after.confirmFun()} className='btn py-3 px-5'>
+            <button onClick={() => after.confirmFun()} className='btn px-2 py-1'>
               确认
             </button>
           )}
