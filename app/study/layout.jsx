28f03b09467f1bec9dfa5a8f741ceca9ca84ad3f
MR-Addict <2750417853@qq.com>
2023-01-04 16:45:21 +0800

    update NEXTJS 13.0.6 to 13.1.1

--- app/study/layout.jsx
+++ app/study/layout.jsx
@@ -2,7 +2,7 @@ import Sidebar from "./Sidebar";
 
 export default function ScheduleLayout({ children }) {
   return (
-    <div className='w-full h-full frame flex flex-col md:flex-row gap-10'>
+    <div className='w-full flex-1 frame flex flex-col md:flex-row gap-10'>
       <Sidebar />
       {children}
     </div>
