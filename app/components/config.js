956fc5b21a4815763db225fdf790a80084b0b317
MR-Addict <2750417853@qq.com>
2023-01-07 21:38:33 +0800

    update home page link

--- app/components/config.js
+++ app/components/config.js
@@ -3,21 +3,21 @@ import { FcElectronics, FcOrgUnit, FcMindMap } from "react-icons/fc";
 export const stacksData = [
   {
     title: "电控方向",
-    href: "/",
+    href: "/study",
     icon: <FcElectronics />,
     intro:
       "电控方向主要研究电子及其控制技术，主要包括单片机的学习，熟练掌握主流单片机的开发使用，同时也会涉及通信协议，底层驱动和物联网的开发等等。",
   },
   {
     title: "算法方向",
-    href: "/",
+    href: "/study",
     icon: <FcMindMap />,
     intro:
       "算法方向主要是学习借助计算机编程语言，使用基于数学或者其他学科的知识，构建出解决数学或者实际问题的算法方案等等。",
   },
   {
     title: "视觉方向",
-    href: "/",
+    href: "/study",
     icon: <FcOrgUnit />,
     intro: "视觉方向主要是学习借助计算机视觉，包括主流的视觉算法以及AI，从图像中抽离获取信息以及对图像的处理识别等等。",
   },
