90caf88bc1a1efd27409e2f88b60181dec64d122
MR-Addict <2750417853@qq.com>
2023-01-09 22:10:58 +0800

    chang study home page to recommand

--- app/study/[studyid1]/page.jsx
+++ app/study/[studyid1]/page.jsx
@@ -10,14 +10,11 @@ export default async function Page({ params: { studyid1 } }) {
   const data = await res.json();
   const lessons = data.data;
   const prefix = data.prefix;
+
   return (
     <div className='w-full flex flex-col gap-5'>
       <div className='w-full flex flex-row items-center justify-between'>
         <div className='flex flex-row items-center justify-start gap-1'>
-          <Link href='/study' className='title text-xl text-theme hover:underline'>
-            所有课程
-          </Link>
-          <h1>/</h1>
           <Link href={`/study/${decode_studyid1}`} className='title text-xl text-theme hover:underline'>
             {sidebarData.find((item) => item.path === decode_studyid1).title}
           </Link>
--- app/study/page.jsx
+++ app/study/page.jsx
@@ -3,17 +3,18 @@ import Link from "next/link";
 import { CourseCard, Upload } from "./components";
 
 export default async function Page() {
-  const res = await fetch(`${process.env.BACKEND_URL}study/getall`, { cache: "no-store" });
+  const res = await fetch(`${process.env.BACKEND_URL}study/getrecommend`, { cache: "no-store" });
   if (!res.ok) throw new Error("Failed to fetch data");
   const data = await res.json();
   const lessons = data.data;
   const prefix = data.prefix;
+
   return (
     <div className='w-full flex flex-col gap-5'>
       <div className='w-full flex flex-row items-center justify-between'>
-        <Link href='/study' className='w-fit title text-xl text-theme hover:underline'>
-          所有课程
-        </Link>
+        <h1 href='/study' className='w-fit title text-xl'>
+          推荐课程
+        </h1>
         <Upload />
       </div>
       <div className='grid gap-5 grid-cols-1 md:grid-cols-3'>
