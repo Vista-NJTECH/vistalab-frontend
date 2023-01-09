399a5561b32829df8dc34d7565fbf6603f16c514
MR-Addict <2750417853@qq.com>
2023-01-09 23:26:19 +0800

    change study page to client page

--- app/study/page.jsx
+++ app/study/page.jsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { useState, useEffect } from "react";
+
+import { Spin } from "../../components";
 import { CourseCard, Upload } from "./components";
 
 export default function Page() {
   const [currentPage, setCurrentPage] = useState(1);
   const [allPagesNum, setAllPagesNum] = useState(1);
   const [lessons, setLessons] = useState({ data: [] });
+  const [isLoading, setIsloading] = useState(false);
 
   async function fetchStudyData(page) {
+    setIsloading(true);
     await fetch(`${process.env.BACKEND_URL}study/getall?page=${page}`)
       .then((res) => res.json())
       .then((data) => {
@@ -18,8 +22,12 @@ export default function Page() {
         } else {
           console.error(data.message);
         }
+        setIsloading(false);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        console.error(error);
+        setIsloading(false);
+      });
   }
 
   useEffect(() => {
@@ -34,11 +42,15 @@ export default function Page() {
         </h1>
         <Upload />
       </div>
-      <div className='grid gap-5 grid-cols-1 md:grid-cols-3'>
-        {lessons.data.map((item, index) => (
-          <CourseCard key={index} prefix={lessons.prefix} course={item} />
-        ))}
-      </div>
+      {isLoading ? (
+        <Spin />
+      ) : (
+        <div className='grid gap-5 grid-cols-1 md:grid-cols-3'>
+          {lessons.data.map((item, index) => (
+            <CourseCard key={index} prefix={lessons.prefix} course={item} />
+          ))}
+        </div>
+      )}
       <div className='w-full flex flex-row items-center justify-center gap-2'>
         {[...Array(allPagesNum)].map((item, index) => (
           <button
--- components/index.js
+++ components/index.js
@@ -2,4 +2,5 @@ export { default as ScrollToTop } from "./ScrollToTop/ScrollToTop";
 export { default as Footer } from "./Footer/Footer";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Popup } from "./Popup/Popup";
+export { default as Spin } from "./Spin/Spin";
 export { NextauthProvider, ContextProvider, useStateContext } from "./Provider/Provider";
