b6ad63c548f02669002816286134ffb7c5fd0a44
MR-Addict <2750417853@qq.com>
2023-03-22 00:12:05 +0800

    update logo

--- app/head.jsx
+++ app/head.jsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>远景实验室</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <meta name='description' content='远景实验室主页' />
-      <link rel='icon' href='/favicon.png' />
+      <link rel='icon' href='/favicon.ico' />
     </>
   );
 }
