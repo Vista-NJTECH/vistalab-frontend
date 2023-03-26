1e8c1806ed4a5817c4e15c68ab17f83f4fe65862
MR-Addict <2750417853@qq.com>
2023-03-27 00:09:43 +0800

    fix build eror

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,12 +4,12 @@ import FaceLogin from "./FaceLogin";
 import CredentialsLogin from "./CredentialsLogin";
 import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
-export default async function Page({ searchParams }) {
+export default async function Page() {
   const session = await unstable_getServerSession(authOptions);
 
-  if (session) return <h1 className='title text-2xl'>你已经登录，请退出后再登录</h1>;
+  if (session) return <h1 className='title text-2xl'>你已经登录，不能访问此页面</h1>;
 
-  const isFaceLogin = searchParams.isFaceLogin ? JSON.parse(searchParams.isFaceLogin.toLowerCase()) : false;
+  const isFaceLogin = false;
 
   if (isFaceLogin) return <FaceLogin />;
   else return <CredentialsLogin />;
