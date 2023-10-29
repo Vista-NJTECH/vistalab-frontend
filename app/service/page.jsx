8009afc02c049065436834d3e77fad2ca8e91184
JerryGu <jerrygu.gjw@gmail.com>
2023-10-29 13:02:11 +0000

    change pdf

--- app/service/page.jsx
+++ app/service/page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MyPDFPage = () => {
   return (
-    <embed src={process.env.BACKEND_URL + 'files/files/server.pdf'} type="application/pdf" width="100%" height="600px" />
+    <embed src={process.env.BACKEND_URL + 'files/files/server.pdf'} type="application/pdf" width="100%" height="1200px" />
   );
 };
 
